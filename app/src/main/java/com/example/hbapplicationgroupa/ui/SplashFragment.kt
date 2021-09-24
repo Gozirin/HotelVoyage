13368f912d57665a0c1f1286821d75eb71ad76c1
gentlekboy <gentlekboy@gmail.com>
2021-09-25 00:31:38 +0100

    added all fragments required to build screens

--- app/src/main/java/com/example/hbapplicationgroupa/ui/SplashFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/ui/SplashFragment.kt
@@ -5,16 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.hbapplicationgroupa.databinding.FragmentPaymentCheckoutBinding
+import com.example.hbapplicationgroupa.databinding.FragmentSplashScreenBinding
 
-class PaymentCheckoutFragment : Fragment() {
+class SplashFragment : Fragment() {
     //Set up view binding here
-    private var _binding: FragmentPaymentCheckoutBinding? = null
+    private var _binding: FragmentSplashScreenBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         //Enabled view binding here
-        _binding = FragmentPaymentCheckoutBinding.inflate(inflater, container, false)
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
         return binding.root
     }
 
