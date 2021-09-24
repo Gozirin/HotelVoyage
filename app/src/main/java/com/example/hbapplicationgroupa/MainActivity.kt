8e879eeaa143be53e253b51251d1f0053590ffb2
Godday Okoduwa <goddayokos@gmail.com>
2021-09-24 01:03:27 +0100

    Fixed Error

--- app/src/main/java/com/example/hbapplicationgroupa/MainActivity.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.hbapplicationgroupa
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity:AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-                        super.onCreate(savedInstanceState)
-                    setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
