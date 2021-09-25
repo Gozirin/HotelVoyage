0a05b6382b0bf1ffebfc84a655af74857158bde4
Abass14 <abasstoy14@gmail.com>
2021-09-25 22:00:19 +0100

    added a model package alongside HotelRoomServiceModel data class

--- app/src/main/java/com/example/hbapplicationgroupa/adapter/HotelRoomServiceViewPagerAdapter.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/adapter/HotelRoomServiceViewPagerAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.hbapplicationgroupa.adapter
 
-class HotelRoomServiceViewPagerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hbapplicationgroupa.R
+
+/**
+ * This is the ViewPager Adapter for the id:hotel_desc_services_viewPager
+ */
+class HotelRoomServiceViewPagerAdapter() : RecyclerView.Adapter<HotelRoomServiceViewPagerAdapter.HotelViewHolder>() {
+    class HotelViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HotelViewHolder {
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.hotel_services_viewpager_layout, parent, false)
+        return HotelViewHolder(inflater)
+    }
+
+    override fun onBindViewHolder(holder: HotelViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
---
+++ app/src/main/java/com/example/hbapplicationgroupa/model/HotelRoomServiceModel.kt
@@ -0,0 +1,6 @@
+package com.example.hbapplicationgroupa.model
+
+data class HotelRoomServiceModel(
+    val roomImage: Int,
+    val roomTitle: String
+)
