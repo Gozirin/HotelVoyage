48ca0d5e129f2a939e07a85b70573156c8bff179
Abass14 <abasstoy14@gmail.com>
2021-09-25 23:05:37 +0100

    added HorizontalMarginItemDecoration class for hotel room viewpager animation

--- app/src/main/java/com/example/hbapplicationgroupa/ui/HotelDescription2Fragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/ui/HotelDescription2Fragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.hbapplicationgroupa.HorizontalMarginItemDecoration
+import com.example.hbapplicationgroupa.R
 import com.example.hbapplicationgroupa.adapter.HotelRoomServiceViewPagerAdapter
 import com.example.hbapplicationgroupa.databinding.FragmentHotelDescription2Binding
 import com.example.hbapplicationgroupa.model.HotelRoomServiceModel
@@ -28,6 +31,7 @@ class HotelDescription2Fragment : Fragment() {
         hotelRoomList = HotelRoomServiceModel.roomDataList
         hotelRoomServiceViewPagerAdapter = HotelRoomServiceViewPagerAdapter(hotelRoomList)
         initHotelRoomServiceViewPager()
+        animateHotelRoomServiceViewPager()
     }
 
     //Method initializing HotelRoomServiceViewPager attributes
@@ -40,5 +44,22 @@ class HotelDescription2Fragment : Fragment() {
     //Method for animating HotelRoomServiceViewPager
     private fun animateHotelRoomServiceViewPager(){
         binding.hotelDescServicesViewPager.offscreenPageLimit = 1
+
+        val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
+        val currentItemHorizontalMarginPx = resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+        val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+        val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
+            page.translationX = -pageTranslationX * position
+            // Scaling the item's height.
+            page.scaleY = 1 - (0.25f * Math.abs(position))
+        }
+        binding.hotelDescServicesViewPager.setPageTransformer(pageTransformer)
+        // The ItemDecoration gives the current (centered) item horizontal margin so that
+        // it doesn't occupy the whole screen width. Without it the items overlap
+        val itemDecoration = HorizontalMarginItemDecoration(
+            requireContext(),
+            R.dimen.viewpager_current_item_horizontal_margin
+        )
+        binding.hotelDescServicesViewPager.addItemDecoration(itemDecoration)
     }
 }
