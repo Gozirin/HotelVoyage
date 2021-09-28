932cf385aa8b0f2111546d74be743fb60ddc0bfd
gentlekboy <gentlekboy@gmail.com>
2021-09-28 17:57:52 +0100

    implemented showing and hiding of bottom sheets

--- app/src/main/java/com/example/hbapplicationgroupa/ui/BookingDetailsFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/ui/BookingDetailsFragment.kt
@@ -1,11 +1,19 @@
 package com.example.hbapplicationgroupa.ui
 
 import android.os.Bundle
+import android.util.SparseIntArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.aminography.primecalendar.civil.CivilCalendar
+import com.aminography.primedatepicker.common.BackgroundShapeType
+import com.aminography.primedatepicker.picker.PrimeDatePicker
+import com.aminography.primedatepicker.picker.theme.LightThemeFactory
+import com.example.hbapplicationgroupa.R
 import com.example.hbapplicationgroupa.databinding.FragmentBookingDetailsBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.*
 
 class BookingDetailsFragment : Fragment() {
     //Set up view binding here
@@ -18,8 +26,84 @@ class BookingDetailsFragment : Fragment() {
         return binding.root
     }
 
-    //TODO: UI manipulation can be done here
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.checkInEditText.setOnClickListener {
+            showDateRangePicker()
+        }
+
+        binding.checkOutEditText.setOnClickListener {
+            showDateRangePicker()
+        }
+
+        binding.peopleEditText.setOnClickListener {
+            peopleBottomSheet()
+        }
+
+        binding.roomsEditText.setOnClickListener {
+            roomsBottomSheet()
+        }
+    }
+
+    private fun peopleBottomSheet(){
+        val bottomSheet = context?.let { BottomSheetDialog(it) }
+        val view = layoutInflater.inflate(R.layout.fragment_number_of_people_bottom_sheet_dialog, null)
+        bottomSheet?.setContentView(view)
+        bottomSheet?.show()
+    }
+
+    private fun roomsBottomSheet(){
+        val bottomSheet = context?.let { BottomSheetDialog(it) }
+        val view = layoutInflater.inflate(R.layout.fragment_number_of_rooms_bottom_sheet_dialog, null)
+        bottomSheet?.setContentView(view)
+        bottomSheet?.show()
+    }
+
+    private val themeFactory = object : LightThemeFactory(){
+        override val pickedDayBackgroundShapeType: BackgroundShapeType
+            get() = BackgroundShapeType.CIRCLE
+
+        override val calendarViewPickedDayBackgroundColor: Int
+            get() = getColor(R.color.splash_screen_background_color)
+
+        override val calendarViewPickedDayInRangeBackgroundColor: Int
+            get() = getColor(R.color.red)
+
+        override val calendarViewPickedDayInRangeLabelTextColor: Int
+            get() = getColor(R.color.black)
+
+        override val calendarViewWeekLabelTextColors: SparseIntArray
+            get() = SparseIntArray(7).apply {
+                val black = getColor(android.R.color.black)
+                put(Calendar.SATURDAY, black)
+                put(Calendar.SUNDAY, black)
+                put(Calendar.MONDAY, black)
+                put(Calendar.TUESDAY, black)
+                put(Calendar.WEDNESDAY, black)
+                put(Calendar.THURSDAY, black)
+                put(Calendar.FRIDAY, black)
+            }
+
+        override val calendarViewShowAdjacentMonthDays: Boolean
+            get() = true
+
+        override val selectionBarBackgroundColor: Int
+            get() = getColor(R.color.splash_screen_background_color)
+
+        override val selectionBarRangeDaysItemBackgroundColor: Int
+            get() = getColor(R.color.splash_screen_background_color)
+    }
+
+    private fun showDateRangePicker() {
+        val today = CivilCalendar(TimeZone.getDefault(), Locale.getDefault())
+        val datePicker = PrimeDatePicker.bottomSheetWith(today)
+            .pickRangeDays { startDay, endDay ->
+
+            }.applyTheme(themeFactory).build()
+
+        fragmentManager?.let {
+            datePicker.show(it, "dateRange")
+        }
     }
 }
