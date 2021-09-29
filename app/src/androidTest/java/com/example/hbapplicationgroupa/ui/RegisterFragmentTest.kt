f747798199650e6affd8f0c411df5da1452b1f02
MohammedQuraysh <mohammedquraysh4@gmail.com>
2021-09-29 15:17:11 +0100

    imported fragmentScenario class

--- app/src/androidTest/java/com/example/hbapplicationgroupa/ui/RegisterFragmentTest.kt
+++ app/src/androidTest/java/com/example/hbapplicationgroupa/ui/RegisterFragmentTest.kt
@@ -1,5 +1,12 @@
 package com.example.hbapplicationgroupa.ui
 
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 
-class RegisterFragmentTest
+@RunWith(AndroidJUnit4::class)
+class RegisterFragmentTest{
+    private lateinit var scenario: FragmentScenario<LoginFragment>
+
+}
