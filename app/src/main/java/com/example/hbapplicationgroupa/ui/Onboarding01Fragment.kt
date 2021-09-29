1cc7a77f1c704203859a6bbbc460d4fd2cf08b71
NenyeIkpa <82512623+NenyeIkpa@users.noreply.github.com>
2021-09-29 15:30:37 +0100

    updated click events and added comments

--- app/src/main/java/com/example/hbapplicationgroupa/ui/Onboarding01Fragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/ui/Onboarding01Fragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
-import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -25,7 +24,11 @@ class Onboarding01Fragment : Fragment() {
     private lateinit var onBoardingList: MutableList<OnBoardingInfo>
     private lateinit var onBoardingAdapter: OnBoardingViewPagerAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         //Enabled view binding here
         _binding = FragmentOnboarding01Binding.inflate(inflater, container, false)
         return binding.root
@@ -38,13 +41,14 @@ class Onboarding01Fragment : Fragment() {
         setUpOnBoardingPage()
         setUpIndicator()
         onClickOptionEvent()
-        onClickChangeViewEvent()
     }
 
     //create method to set up the onBoarding pages
     private fun setUpOnBoardingPage() {
-        onBoardingAdapter = OnBoardingViewPagerAdapter()
-        binding.viewpager2.adapter = onBoardingAdapter
+        onBoardingAdapter = OnBoardingViewPagerAdapter()    //initialise onboarding adapter
+        binding.viewpager2.adapter = onBoardingAdapter         //assign the viewpager2 adapter to the onboarding adapter
+
+        //initialise the onBoarding list using the model class onBoardingInfo. The onBoardingList contains the data to be listed in the viewpager
         onBoardingList = mutableListOf(
             OnBoardingInfo(
                 R.drawable.onboarding_01_img,
@@ -63,29 +67,24 @@ class Onboarding01Fragment : Fragment() {
             ),
         )
 
-        onBoardingAdapter.onBoardingInfo = onBoardingList
+        onBoardingAdapter.onBoardingInfo = onBoardingList       //set the onBoardingList to the adapter
 
-        binding.viewpager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+        binding.viewpager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 isCurrentIndicator(position)
                 textChangeOnClickEvent(position)
+                onClickChangeViewEvent(position)
             }
-        } )
-        (binding.viewpager2.getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
-
-//        binding.fragmentOnBoarding01OptionTv.setOnClickListener {
-//            if (binding.viewpager2.currentItem + 1 < onBoardingAdapter.itemCount) {
-//                binding.viewpager2.currentItem += 1
-//            }else {
-//                navigateToRegisterFragment()
-//            }
-//        }
+        })
+        (binding.viewpager2.getChildAt(0) as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_NEVER
     }
-
+        //setup indicator
     private fun setUpIndicator() {
         val indicator = arrayOfNulls<ImageView>(onBoardingAdapter.itemCount)
-        val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
         layoutParams.setMargins(12, 0, 12, 8)
 
         for (eachCircle in indicator.indices) {
@@ -104,18 +103,18 @@ class Onboarding01Fragment : Fragment() {
         }
 
     }
-
+//set default text in views
     private fun defaultOnClickViewText() {
         binding.fragmentOnBoarding01OptionTv.text = "Skip"
         binding.fragmentOnBoarding01ChangeViewButton.text = "Next"
     }
-
+//set text when view is changed
     private fun changeOnClickViewText() {
         binding.fragmentOnBoarding01OptionTv.text = "Done"
         binding.fragmentOnBoarding01ChangeViewButton.text = "Get Started"
     }
-
-    fun  isCurrentIndicator(position: Int) {
+//set the indicator light to the current page
+    fun isCurrentIndicator(position: Int) {
         val childCount = binding.circleIndicator.childCount
         for (spot in 0 until childCount) {
             val view = binding.circleIndicator.getChildAt(spot) as ImageView
@@ -127,7 +126,7 @@ class Onboarding01Fragment : Fragment() {
                         )
                     }
                 )
-            }else {
+            } else {
                 view.setImageDrawable(
                     context?.let {
                         ContextCompat.getDrawable(
@@ -138,28 +137,34 @@ class Onboarding01Fragment : Fragment() {
             }
         }
     }
-
+//set function to display required text on click event
     fun textChangeOnClickEvent(position: Int) {
         if (position < onBoardingAdapter.itemCount - 1) {
             defaultOnClickViewText()
-        }else {
+        } else {
             changeOnClickViewText()
         }
     }
-
+//function to navigates to register fragment
     private fun navigateToRegisterFragment() {
         findNavController().navigate(R.id.action_onboarding01Fragment2_to_registerFragment)
     }
-     private fun onClickOptionEvent() {
-         binding.fragmentOnBoarding01OptionTv.setOnClickListener {
-                 navigateToRegisterFragment()
-         }
-     }
-    private fun onClickChangeViewEvent() {
-        binding.viewpager2.apply {
-            beginFakeDrag()
-            fakeDragBy(-10f)
-            endFakeDrag()
+
+        //function that effects changes on click of options view
+    private fun onClickOptionEvent() {
+        binding.fragmentOnBoarding01OptionTv.setOnClickListener {
+            navigateToRegisterFragment()
+        }
+    }
+
+    //function that effects changes on click of change view button
+    fun onClickChangeViewEvent(position: Int) {
+        binding.fragmentOnBoarding01ChangeViewButton.setOnClickListener {
+            if (position == onBoardingAdapter.itemCount - 1) {
+                navigateToRegisterFragment()
+            } else {
+                binding.viewpager2.setCurrentItem(binding.viewpager2.currentItem + 1, true)
+            }
         }
     }
 }
