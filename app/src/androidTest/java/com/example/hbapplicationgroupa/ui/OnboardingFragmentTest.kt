4ffbc12f07f4dc86eaa180a14f46de666d1eb2e9
NenyeIkpa <82512623+NenyeIkpa@users.noreply.github.com>
2021-09-29 21:43:06 +0100

    updated mockito dependency

--- app/src/androidTest/java/com/example/hbapplicationgroupa/ui/OnboardingFragmentTest.kt
+++ app/src/androidTest/java/com/example/hbapplicationgroupa/ui/OnboardingFragmentTest.kt
@@ -10,10 +10,10 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.hbapplicationgroupa.R
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 
 @RunWith(AndroidJUnit4::class)
