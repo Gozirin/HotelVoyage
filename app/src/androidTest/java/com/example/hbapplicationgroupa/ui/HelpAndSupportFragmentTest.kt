b9f9f342fff815cc055f0573094e6229a66db015
codeessed <gbohunmimakinde@gmail.com>
2021-09-29 16:52:36 +0100

    pulled commit

--- app/src/androidTest/java/com/example/hbapplicationgroupa/ui/HelpAndSupportFragmentTest.kt
+++ app/src/androidTest/java/com/example/hbapplicationgroupa/ui/HelpAndSupportFragmentTest.kt
@@ -3,15 +3,12 @@ package com.example.hbapplicationgroupa.ui
 import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import com.example.hbapplicationgroupa.R
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
