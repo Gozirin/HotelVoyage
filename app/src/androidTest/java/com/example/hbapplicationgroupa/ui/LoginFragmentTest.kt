76a50dc24d59faf8a5a1c0ef2848edb798e9f5db
MohammedQuraysh <mohammedquraysh4@gmail.com>
2021-09-29 16:20:22 +0100

    added fragment scenario class and some id

--- app/src/androidTest/java/com/example/hbapplicationgroupa/ui/LoginFragmentTest.kt
+++ app/src/androidTest/java/com/example/hbapplicationgroupa/ui/LoginFragmentTest.kt
@@ -1,5 +1,32 @@
 package com.example.hbapplicationgroupa.ui
 
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.hbapplicationgroupa.R
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class LoginFragmentTest
+@RunWith(AndroidJUnit4::class)
+class LoginFragmentTest{
+
+    private lateinit var scenario: FragmentScenario<LoginFragment>
+
+    @Before
+    fun setUp(){
+        scenario = launchFragmentInContainer(themeResId = R.style.Theme_HBApplicationGroupA)
+    }
+    @Test
+    fun check_if_data_is_inValid(){
+        Espresso.onView(withId(R.id.tvEmailTe_login_screen)).perform(ViewActions.typeText("mohammed4@gmail.com"), ViewActions.closeSoftKeyboard())
+        Espresso.onView(withId(R.id.tvEditPassword_login_screen)).perform(ViewActions.typeText("08130228099"), ViewActions.closeSoftKeyboard())
+        Espresso.onView(withId(R.id.btn_login_screen)).perform(ViewActions.click())
+    }
+
+
+}
