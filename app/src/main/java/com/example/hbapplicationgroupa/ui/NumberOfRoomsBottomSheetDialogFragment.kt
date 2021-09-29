2aa97120320400926b45ff2e0b6283e3d5cebf91
gentlekboy <gentlekboy@gmail.com>
2021-09-29 13:55:34 +0100

    implemented number of rooms bottom sheet for BookingDetailsFragment.kt

--- app/src/main/java/com/example/hbapplicationgroupa/ui/NumberOfRoomsBottomSheetDialogFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/ui/NumberOfRoomsBottomSheetDialogFragment.kt
@@ -1,15 +1,50 @@
 package com.example.hbapplicationgroupa.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.hbapplicationgroupa.R
+import com.example.hbapplicationgroupa.RoomNumberBottomSheetAdapter
+import com.example.hbapplicationgroupa.RoomNumberBottomSheetData
+import com.example.hbapplicationgroupa.databinding.FragmentNumberOfRoomsBottomSheetDialogBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class NumberOfRoomsBottomSheetDialogFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_number_of_rooms_bottom_sheet_dialog, container, false)
+class NumberOfRoomsBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentNumberOfRoomsBottomSheetDialogBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var adapter: RoomNumberBottomSheetAdapter
+    private lateinit var listOfRooms: MutableList<RoomNumberBottomSheetData>
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentNumberOfRoomsBottomSheetDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listOfRooms = mutableListOf(
+            RoomNumberBottomSheetData("Room No. 1", true),
+            RoomNumberBottomSheetData("Room No. 2", false),
+            RoomNumberBottomSheetData("Room No. 6", false),
+            RoomNumberBottomSheetData("Room No. 22", false),
+            RoomNumberBottomSheetData("Room No. 24", false)
+        )
+
+        adapter = RoomNumberBottomSheetAdapter()
+        binding.roomNumberBottomSheetRecyclerView.adapter = adapter
+        binding.roomNumberBottomSheetRecyclerView.setHasFixedSize(true)
+
+        adapter.differ.submitList(listOfRooms)
+
+        Log.d("GKB", "NUMBER OF ROOMS: ${adapter.differ.currentList}")
+
+        binding.cancelButton.setOnClickListener {
+            findNavController().navigate(R.id.action_numberOfRoomsBottomSheetDialogFragment_to_bookingDetailsFragment)
+        }
     }
 }
