8c98369bd4582fe7cc0b2d33636dbd50930895ff
MohammedQuraysh <mohammedquraysh4@gmail.com>
2021-10-05 14:17:31 +0100

    added vieModel extension

--- app/src/main/java/com/example/hbapplicationgroupa/viewModel/HotelAppViewModel.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/viewModel/HotelAppViewModel.kt
@@ -1,9 +1,10 @@
 package com.example.hbapplicationgroupa.viewModel
 
+import androidx.lifecycle.ViewModel
 import com.example.hbapplicationgroupa.repository.HotelAppRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HotelAppViewModel @Inject constructor(var repository: HotelAppRepository) {
+class HotelAppViewModel @Inject constructor(var repository: HotelAppRepository): ViewModel() {
 }
