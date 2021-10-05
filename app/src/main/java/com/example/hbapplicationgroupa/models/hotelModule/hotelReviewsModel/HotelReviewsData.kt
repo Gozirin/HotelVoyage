a6ca9680b3cfbb72dfc209013422f9768ef3359a
gentlekboy <gentlekboy@gmail.com>
2021-10-05 22:27:55 +0100

    added primary key

--- app/src/main/java/com/example/hbapplicationgroupa/models/hotelModule/hotelReviewsModel/HotelReviewsData.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/models/hotelModule/hotelReviewsModel/HotelReviewsData.kt
@@ -1,6 +1,10 @@
 package com.example.hbapplicationgroupa.models.hotelModule.hotelReviewsModel
 
+import androidx.room.PrimaryKey
+
 data class HotelReviewsData(
+    @PrimaryKey(autoGenerate = true)
+    val tableNumber: Int,
     val review: String,
     val reviewText: String,
     val customerId: String,
