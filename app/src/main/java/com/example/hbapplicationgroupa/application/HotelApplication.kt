afaa34a19a399ed38fb9f438e68cda43a13ce2e9
obetta1 <obettabenjamin44@gmail.com>
2021-10-06 17:23:02 +0100

    i

--- app/src/main/java/com/example/hbapplicationgroupa/application/HotelApplication.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/application/HotelApplication.kt
@@ -1,8 +1,19 @@
 package com.example.hbapplicationgroupa.application
 
 import android.app.Application
+import com.example.hbapplicationgroupa.database.HotelDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HotelApplication : Application() {
+
+
+
+    companion object{
+        private lateinit var instance:HotelApplication
+
+        private val database : HotelDatabase by lazy {
+            HotelDatabase.getDbInstance(instance)
+        }
+    }
 }
--- app/src/main/java/com/example/hbapplicationgroupa/application_module/HotelApplicationModule.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/application_module/HotelApplicationModule.kt
@@ -1,16 +1,10 @@
-package com.example.hbapplicationgroupa.application.application_module
+package com.example.hbapplicationgroupa.application_module
 
 
 
-import com.example.hbapplicationgroupa.network.HotelRetrofitServices
-import com.google.gson.GsonBuilder
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
