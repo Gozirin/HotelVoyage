2bdb5c6de8749cf1b17f92f62b0acc7abc97cf3c
NenyeIkpa <82512623+NenyeIkpa@users.noreply.github.com>
2021-10-06 01:22:26 +0100

    updated retrofit

--- app/src/main/java/com/example/hbapplicationgroupa/network/RetrofitModule.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/network/RetrofitModule.kt
@@ -11,61 +11,64 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitModule {
 
-    private const val BASE_URL = "https://<base_url>/api/v1/"
+class NetworkModule {
 
+    object RetrofitModule {
 
-    @Singleton
-    @Provides
-    //get retrofit instance
-    private val retrofit by lazy {
-        //okhttp interceptor is used to to log retrofit responses especially when debugging.
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .build()
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-    }
+        private const val BASE_URL = "https://<base_url>/api/v1/"
 
-
-    //get api instance from retrofit
-
-    @Singleton
-    @Provides
-    val hbAdminModuleApi: HBAdminModuleApi by lazy {
-        retrofit.create(HBAdminModuleApi::class.java)
-    }
-
-    @Singleton
-    @Provides
-    val hbAmenitiesModuleApi: HBAmenitiesModuleApi by lazy {
-        retrofit.create(HBAmenitiesModuleApi::class.java)
-    }
+        @Singleton
+        @Provides
+        //get retrofit instance
+        private val retrofit by lazy {
+            //okhttp interceptor is used to to log retrofit responses especially when debugging.
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
 
 
-    @Singleton
-    @Provides
-    val hbAuthenticationModuleApi: HBAuthenticationModuleApi by lazy {
-        retrofit.create(HBAuthenticationModuleApi::class.java)
-    }
+        //get api instance from retrofit
+
+        @Singleton
+        @Provides
+        val hbAdminModuleApi: HBAdminModuleApi by lazy {
+            retrofit.create(HBAdminModuleApi::class.java)
+        }
+
+        @Singleton
+        @Provides
+        val hbAmenitiesModuleApi: HBAmenitiesModuleApi by lazy {
+            retrofit.create(HBAmenitiesModuleApi::class.java)
+        }
 
 
-    @Singleton
-    @Provides
-    val hbCustomerModuleApi: HBCustomerModuleApi by lazy {
-        retrofit.create(HBCustomerModuleApi::class.java)
-    }
+        @Singleton
+        @Provides
+        val hbAuthenticationModuleApi: HBAuthenticationModuleApi by lazy {
+            retrofit.create(HBAuthenticationModuleApi::class.java)
+        }
 
 
-    @Singleton
-    @Provides
-    val hbHotelModuleApi: HBHotelModuleApi by lazy {
-        retrofit.create(HBHotelModuleApi::class.java)
+        @Singleton
+        @Provides
+        val hbCustomerModuleApi: HBCustomerModuleApi by lazy {
+            retrofit.create(HBCustomerModuleApi::class.java)
+        }
+
+
+        @Singleton
+        @Provides
+        val hbHotelModuleApi: HBHotelModuleApi by lazy {
+            retrofit.create(HBHotelModuleApi::class.java)
+        }
     }
 }
