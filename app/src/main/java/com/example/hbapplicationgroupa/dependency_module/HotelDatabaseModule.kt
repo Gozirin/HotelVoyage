e450ff5391e8f73a0e0f069302304024dac94a96
gentlekboy <gentlekboy@gmail.com>
2021-10-10 21:57:07 +0100

    provided database and DAOs in DI module for injection

--- app/src/main/java/com/example/hbapplicationgroupa/dependency_module/HotelDatabaseModule.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/dependency_module/HotelDatabaseModule.kt
@@ -1,67 +1,47 @@
 package com.example.hbapplicationgroupa.dependency_module
 
-
-import android.content.Context
-import androidx.room.Room
+import android.app.Application
 import com.example.hbapplicationgroupa.database.HotelDatabase
-import com.example.hbapplicationgroupa.database.dao.*
+import com.example.hbapplicationgroupa.database.dao.BookingByUserIdDao
+import com.example.hbapplicationgroupa.database.dao.UserByIdDao
+import com.example.hbapplicationgroupa.database.dao.UserPhotoByIdDao
+import com.example.hbapplicationgroupa.database.dao.WishlistByPageNumberDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object HotelDatabaseModule {
-
+class HotelDatabaseModule {
     @Singleton
     @Provides
-    fun provideGetDbInstance(@ApplicationContext context: Context): HotelDatabase {
-        return Room.databaseBuilder(context, HotelDatabase::class.java, "hotelDb")
-            .build()
+    fun provideDatabaseInstance(context: Application): HotelDatabase{
+        return HotelDatabase.getDbInstance(context)
     }
 
     @Singleton
     @Provides
-    fun provideGetWishListDao(hotelDatabase: HotelDatabase): WishListDao {
-        return hotelDatabase.getWishListDao()
+    fun provideBookingByUserIdDao(hotelDatabase: HotelDatabase): BookingByUserIdDao {
+        return hotelDatabase.getBookingByUserIdDao()
     }
 
     @Singleton
     @Provides
-    fun provideGetCustomersBookingDao(hotelDatabase: HotelDatabase): CustomerBookingDao {
-        return hotelDatabase.getCustomerBookingDao()
+    fun provideWishlistByPageNumberDao(hotelDatabase: HotelDatabase): WishlistByPageNumberDao {
+        return hotelDatabase.getWishlistByPageNumberDao()
     }
 
     @Singleton
     @Provides
-    fun provideGetAmenitiesDao(hotelDatabase: HotelDatabase): AmenitiesDao {
-        return hotelDatabase.getAmenitiesDao()
+    fun provideUserByUserIdDao(hotelDatabase: HotelDatabase): UserByIdDao {
+        return hotelDatabase.getUserByIdDao()
     }
 
     @Singleton
     @Provides
-    fun provideGetHotelRoomByIdDao(hotelDatabase: HotelDatabase): HotelRoomByIdDao {
-        return hotelDatabase.getHotelRoomByIdDao()
-    }
-
-    @Singleton
-    @Provides
-    fun provideGetHotelDataDao(hotelDatabase: HotelDatabase): HotelDao {
-        return hotelDatabase.getHotelDataDao()
-    }
-
-    @Singleton
-    @Provides
-    fun provideGetHotelRatingsDao(hotelDatabase: HotelDatabase): HotelRatingsDao {
-        return hotelDatabase.getHotelRatingsDao()
-    }
-
-    @Singleton
-    @Provides
-    fun provideGetUserHotelDao(hotelDatabase: HotelDatabase): UserHotelDao {
-        return hotelDatabase.getUserHotelDao()
+    fun provideUserPhotoByIdDao(hotelDatabase: HotelDatabase): UserPhotoByIdDao {
+        return hotelDatabase.getUserPhotoByIdDao()
     }
 }
