a7483c4cacc6a86f8e5a70aec7fc745504778af9
gentlekboy <gentlekboy@gmail.com>
2021-10-10 22:25:00 +0100

    refactored codes

--- app/src/main/java/com/example/hbapplicationgroupa/dependency_module/RetrofitModule.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/dependency_module/RetrofitModule.kt
@@ -5,6 +5,7 @@ import com.example.hbapplicationgroupa.network.hotelbookingapi.HBAdminModuleApi
 import com.example.hbapplicationgroupa.network.hotelbookingapi.HBAmenitiesModuleApi
 import com.example.hbapplicationgroupa.network.hotelbookingapi.HBCustomerModuleApi
 import com.example.hbapplicationgroupa.network.hotelbookingapi.HBHotelModuleApi
+import com.example.hbapplicationgroupa.utils.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,37 +18,41 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-
 object RetrofitModule {
-
-    private const val BASE_URL = "https://<base_url>/api/v1/"
-
     @Singleton
     @Provides
     //get retrofit instance
+//    fun retrofit(): Retrofit {
+//        //okhttp interceptor is used to to log retrofit responses especially when debugging.
+//        val interceptor = HttpLoggingInterceptor()
+//        interceptor.level = HttpLoggingInterceptor.Level.BODY
+//        val client = OkHttpClient.Builder()
+//            .addInterceptor(interceptor)
+//            .build()
+//        return Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .client(client)
+//            .build()
+//    }
     fun retrofit(): Retrofit {
         //okhttp interceptor is used to to log retrofit responses especially when debugging.
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .build()
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
+            .client(OkHttpClient.Builder().addInterceptor(interceptor).build())
             .build()
     }
 
 
     //get api instance from retrofit
-
     @Singleton
     @Provides
     fun provideHBAdminModuleApi(): HBAdminModuleApi {
         return retrofit().create(HBAdminModuleApi::class.java)
     }
-
 
     @Singleton
     @Provides
@@ -55,20 +60,17 @@ object RetrofitModule {
         return retrofit().create(HBAmenitiesModuleApi::class.java)
     }
 
-
     @Singleton
     @Provides
     fun provideHBAuthenticationModuleApi(): HBAuthenticationModuleApi {
         return retrofit().create(HBAuthenticationModuleApi::class.java)
     }
 
-
     @Singleton
     @Provides
     fun provideHBCustomerModuleApi(): HBCustomerModuleApi {
         return retrofit().create(HBCustomerModuleApi::class.java)
     }
-
 
     @Singleton
     @Provides
