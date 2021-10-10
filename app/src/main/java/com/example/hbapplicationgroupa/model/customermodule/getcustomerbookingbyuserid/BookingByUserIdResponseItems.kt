aa12f28b072d36319f7398f5c4a45a38e3f088c2
gentlekboy <gentlekboy@gmail.com>
2021-10-10 20:20:23 +0100

    added entity annotation to data classes

--- app/src/main/java/com/example/hbapplicationgroupa/model/customermodule/getcustomerbookingbyuserid/BookingByUserIdResponseItems.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/model/customermodule/getcustomerbookingbyuserid/BookingByUserIdResponseItems.kt
@@ -1,5 +1,8 @@
 package com.example.hbapplicationgroupa.model.customermodule.getcustomerbookingbyuserid
 
+import androidx.room.Entity
+
+@Entity(tableName = "bookingByUserId")
 data class BookingByUserIdResponseItems(
     val BookingReference: String,
     val CheckIn: DateTime,
--- app/src/main/java/com/example/hbapplicationgroupa/model/customermodule/getcustomerwishlistbypagenumber/WishlistByPageNumberResponseItems.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/model/customermodule/getcustomerwishlistbypagenumber/WishlistByPageNumberResponseItems.kt
@@ -1,5 +1,8 @@
 package com.example.hbapplicationgroupa.model.customermodule.getcustomerwishlistbypagenumber
 
+import androidx.room.Entity
+
+@Entity(tableName = "wishlistByPageNumber")
 data class WishlistByPageNumberResponseItems(
     val hotelId: String,
     val hotelName: String,
