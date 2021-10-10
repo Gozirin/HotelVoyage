14248e22cad8fe71bc1c9966d7fd770722d00c81
gentlekboy <gentlekboy@gmail.com>
2021-10-10 20:20:39 +0100

    optimized import

--- app/src/main/java/com/example/hbapplicationgroupa/model/customermodule/getcustomerbookingbyuserid/BookingByUserIdResponseModel.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/model/customermodule/getcustomerbookingbyuserid/BookingByUserIdResponseModel.kt
@@ -1,7 +1,5 @@
 package com.example.hbapplicationgroupa.model.customermodule.getcustomerbookingbyuserid
 
-import com.example.hbapplicationgroupa.model.authmodule.loginuser.LoginUserResponse
-
 data class BookingByUserIdResponseModel(
     val statusCode: String,
     val success: Boolean,
