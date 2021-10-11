333e4443c835deda233b88f0efe7f6ff95ebe2c2
gentlekboy <gentlekboy@gmail.com>
2021-10-11 07:28:33 +0100

    added customer, hotel and user module api interfaces

---
+++ app/src/main/java/com/example/hbapplicationgroupa/networksss/UserModuleApiInterface.kt
@@ -0,0 +1,22 @@
+package com.example.hbapplicationgroupa.networksss
+
+import com.example.hbapplicationgroupa.model.usermodule.getuserbyid.GetUserByIdResponseModel
+import com.example.hbapplicationgroupa.model.usermodule.updateuserphotobyuserid.UpdateUserPhotoByUserIdResponseModel
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+
+interface UserModuleApiInterface {
+    @GET("api/v1/Users/get-user/{userId}")
+    suspend fun getUserById(
+        @Path("id") id: String
+    ): Response<GetUserByIdResponseModel>
+
+    @PATCH("api/v1/Users/update-user/{userId}/user-photo")
+    suspend fun updateUserPhotoById(
+        @Path("id") id: String,
+        @Body Photo: Int
+    ): Response<UpdateUserPhotoByUserIdResponseModel>
+}
