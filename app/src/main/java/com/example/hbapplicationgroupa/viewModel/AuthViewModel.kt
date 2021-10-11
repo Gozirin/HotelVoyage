edae9cc41b360345fe8bf5c5a29a032afcb4cfb2
Abass14 <abasstoy14@gmail.com>
2021-10-11 22:04:11 +0100

    added AuthViewModel

---
+++ app/src/main/java/com/example/hbapplicationgroupa/viewModel/AuthViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.hbapplicationgroupa.viewModel
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthViewModel @Inject constructor() {
+}
