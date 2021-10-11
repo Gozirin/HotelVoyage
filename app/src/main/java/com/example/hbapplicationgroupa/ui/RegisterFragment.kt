da565dcc1bd59e3bd9732c7557b64d3e93c3f2ac
MohammedQuraysh <mohammedquraysh4@gmail.com>
2021-10-11 22:24:24 +0100

    Added some text field

--- app/src/main/java/com/example/hbapplicationgroupa/ui/RegisterFragment.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/ui/RegisterFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.hbapplicationgroupa.R
@@ -32,5 +34,32 @@ class RegisterFragment : Fragment() {
         binding.btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
+
+        //  Created an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.gender,
+            R.layout.support_simple_spinner_dropdown_item
+        ).also { adapter ->
+
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            // Apply the adapter to the spinner
+            binding.getSpinner.adapter = adapter
+            binding.getSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    p0?.getItemIdAtPosition(p2)
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                   binding.getSpinner.onItemSelectedListener = this
+
+                }
+
+            }
+        }
+
     }
 }
