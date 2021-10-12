65f4c02bc8ce1520af9b243f1806a9878c1d916d
gentlekboy <gentlekboy@gmail.com>
2021-10-12 03:54:24 +0100

    commented old view model codes

--- app/src/main/java/com/example/hbapplicationgroupa/viewModel/UserHotelDaoViewModel.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/viewModel/UserHotelDaoViewModel.kt
@@ -1,15 +1,15 @@
 package com.example.hbapplicationgroupa.viewModel
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.example.hbapplicationgroupa.models.hotelModule.userHotelsModel.UserHotelData
-import com.example.hbapplicationgroupa.repository.userHotelDaoRepository.UserHotelDaoRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.scopes.ViewModelScoped
-import javax.inject.Inject
-
-@ViewModelScoped
-class UserHotelDaoViewModel
-      @Inject constructor (val userHotelDaoRepository: UserHotelDaoRepository): ViewModel() {
-      val userHotelLiveData: MutableLiveData<List<UserHotelData>> = MutableLiveData()
-}
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+//import com.example.hbapplicationgroupa.models.hotelModule.userHotelsModel.UserHotelData
+//import com.example.hbapplicationgroupa.repository.userHotelDaoRepository.UserHotelDaoRepository
+//import dagger.hilt.android.lifecycle.HiltViewModel
+//import dagger.hilt.android.scopes.ViewModelScoped
+//import javax.inject.Inject
+//
+//@ViewModelScoped
+//class UserHotelDaoViewModel
+//      @Inject constructor (val userHotelDaoRepository: UserHotelDaoRepository): ViewModel() {
+//      val userHotelLiveData: MutableLiveData<List<UserHotelData>> = MutableLiveData()
+//}
