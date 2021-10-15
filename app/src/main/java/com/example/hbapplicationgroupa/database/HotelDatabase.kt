86f8c9ed4d792c5ecf36a1c35d2ccfb932a1de97
gentlekboy <gentlekboy@gmail.com>
2021-10-15 23:19:47 +0100

    made an instance of the database using another method

--- app/src/main/java/com/example/hbapplicationgroupa/database/HotelDatabase.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/database/HotelDatabase.kt
@@ -4,38 +4,60 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.hbapplicationgroupa.database.dao.BookingByUserIdDao
-import com.example.hbapplicationgroupa.database.dao.UserByIdDao
-import com.example.hbapplicationgroupa.database.dao.UserPhotoByIdDao
-import com.example.hbapplicationgroupa.database.dao.WishlistByPageNumberDao
-import com.example.hbapplicationgroupa.model.customermodule.getcustomerbookingbyuserid.BookingByUserIdResponseItems
+import androidx.room.TypeConverters
+import com.example.hbapplicationgroupa.database.dao.*
 import com.example.hbapplicationgroupa.model.customermodule.getcustomerwishlistbypagenumber.WishlistByPageNumberResponseItems
+import com.example.hbapplicationgroupa.model.hotelmodule.gethotelbyid.GetHotelByIdResponseItemData
+import com.example.hbapplicationgroupa.model.typeconverter.TypeConverter
 import com.example.hbapplicationgroupa.model.usermodule.getuserbyid.GetUserByIdResponseItem
 import com.example.hbapplicationgroupa.model.usermodule.updateuserphotobyuserid.UpdateUserPhotoByUserIdResponseItem
 
 //TODO: Add past bookings (entity)
 @Database(entities = [
+    GetHotelByIdResponseItemData::class,
     WishlistByPageNumberResponseItems::class,
     GetUserByIdResponseItem::class,
     UpdateUserPhotoByUserIdResponseItem::class
 ], version = 1, exportSchema = false)
+
+@TypeConverters(TypeConverter::class)
 abstract class HotelDatabase: RoomDatabase() {
 //    abstract fun getBookingByUserIdDao(): BookingByUserIdDao
+    abstract fun getHotelByIdDao(): HotelByIdDao
     abstract fun getWishlistByPageNumberDao(): WishlistByPageNumberDao
     abstract fun getUserByIdDao(): UserByIdDao
     abstract fun getUserPhotoByIdDao(): UserPhotoByIdDao
 
     //Creating a single instance of the database
-    companion object {
+    companion object{
+        @Volatile
         private var dbInstance: HotelDatabase? = null
+        private val lock = Any()
 
-        fun getDbInstance(context: Context): HotelDatabase{
-            if (dbInstance == null){
-                dbInstance = Room.databaseBuilder(context.applicationContext, HotelDatabase::class.java, "hotelDb")
-                    .build()
-            }
-
-            return dbInstance!!
+        operator fun invoke(context: Context) = dbInstance?: synchronized(lock){
+            dbInstance?: getDbInstance(context)
         }
+
+        fun getDbInstance(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            HotelDatabase::class.java,
+            "hotelDb"
+        ).build()
     }
+
+    /*
+    Below is a different way of instantiating our database
+     */
+    //    companion object {
+//        private var dbInstance: HotelDatabase? = null
+//
+//        fun getDbInstance(context: Context): HotelDatabase{
+//            if (dbInstance == null){
+//                dbInstance = Room.databaseBuilder(context.applicationContext, HotelDatabase::class.java, "hotelDb")
+//                    .build()
+//            }
+//
+//            return dbInstance!!
+//        }
+//    }
 }
