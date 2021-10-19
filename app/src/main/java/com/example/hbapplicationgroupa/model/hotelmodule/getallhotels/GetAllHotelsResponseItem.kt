61daba3c49272731ec2fa2618ea4fa2d1bb6ec8e
MohammedQuraysh <mohammedquraysh4@gmail.com>
2021-10-19 17:05:19 +0100

    added new package

--- app/src/main/java/com/example/hbapplicationgroupa/model/hotelmodule/getallhotels/GetAllHotelsResponseItem.kt
+++ app/src/main/java/com/example/hbapplicationgroupa/model/hotelmodule/getallhotels/GetAllHotelsResponseItem.kt
@@ -1,9 +1,15 @@
 package com.example.hbapplicationgroupa.model.hotelmodule.getallhotels
 
+import androidx.room.Entity
+
+@Entity(tableName = "getAllHotel")
 data class GetAllHotelsResponseItem(
     val id: String,
-    val Name: String,
-    val Thumbmail: String,
+    val name: String,
+    val thumbnail: String,
     val Rating: Double,
-    val CheapestRoomPrice: String
+    val CheapestRoomPrice: String,
+    val city: String,
+    val state: String,
+
 )
