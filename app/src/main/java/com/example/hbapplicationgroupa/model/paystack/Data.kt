ee22066dc52a6a73898b1f7dc58145e204f5db34
NenyeIkpa <82512623+NenyeIkpa@users.noreply.github.com>
2021-11-04 04:42:34 +0100

    add files

---
+++ app/src/main/java/com/example/hbapplicationgroupa/model/paystack/Data.kt
@@ -0,0 +1,7 @@
+package com.example.hbapplicationgroupa.model.paystack
+
+data class Data(
+    val access_code: String,
+    val authorization_url: String,
+    val reference: String
+)
---
+++ app/src/main/java/com/example/hbapplicationgroupa/model/paystack/MetadataX.kt
@@ -0,0 +1,5 @@
+package com.example.hbapplicationgroupa.model.paystack
+
+data class MetadataX(
+    val custom_fields: List<CustomField>
+)
